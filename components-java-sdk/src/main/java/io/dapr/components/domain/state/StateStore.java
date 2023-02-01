9bcc7dfbdd2dd36f369efcbaa3e899e07e4e6955
Tiago Alves Macambira <tmacam@burocrata.org>
2023-02-01 09:13:40 -0800

    Support for user-provided StateStore bulk operations.
    Moves bulk state store operations to StateStore interface and define
    default implementations for them. This means that users don't **have**
    to define anything beyond `set`, `get` and `delete` to have a fully
    working State Store **with** bulk-operation support. The default
    implementation of bulk operations rely on single-item operation but users
    are allowed to redefine some or all of those bulk operations in their
    implementation if they so desire.
    
    Closes #15.
    
    Signed-off-by: Tiago Alves Macambira <tmacam@burocrata.org>

--- components-java-sdk/src/main/java/io/dapr/components/domain/state/StateStore.java
+++ components-java-sdk/src/main/java/io/dapr/components/domain/state/StateStore.java
@@ -16,13 +16,77 @@ package io.dapr.components.domain.state;
 import io.dapr.components.aspects.AdvertisesFeatures;
 import io.dapr.components.aspects.InitializableWithProperties;
 import io.dapr.components.aspects.Pingable;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 
+/**
+ * Represents a state store.
+ *
+ * <p>Default implementations are provided for bulk operations. They relly on their
+ * single-item variants and can be made more efficient if your underlying state store
+ * provides native support for bulk operations.</p>
+ */
 public interface StateStore extends InitializableWithProperties, AdvertisesFeatures, Pingable {
+  /**
+   * Get data from the given key.
+   *
+   * @param getRequest Request specifying what to retrieve.
+   * @return A {@link GetResponse} Mono containing the contents associated with the key or
+   *         representing an error.
+   */
   Mono<GetResponse> get(GetRequest getRequest);
 
+  /**
+   * Deletes the specified key from the state store.
+   * @param deleteRequest Request specifying what to delete.
+   * @return An empty Mono representing success or error.
+   */
   Mono<Void> delete(DeleteRequest deleteRequest);
 
+  /**
+   * Sets the value of the specified key.
+   *
+   * @param setRequest Request specifying what key to set and its contents.
+   * @return An empty Mono representing success or error.
+   */
   Mono<Void> set(SetRequest setRequest);
+
+  /**
+   * Retrieves many keys at once.
+   *
+   * @param getRequests list of GetRequest to be performed in bulk.
+   * @return A Flux of GetResponses, one for each requested key.
+   */
+  default Flux<BulkGetStateItem> bulkGet(List<GetRequest> getRequests) {
+    return Flux.fromIterable(getRequests)
+        .map(item -> new BulkGetStateItem(item.key(), this.get(item)));
+  }
+
+  /**
+   * Deletes many keys at once.
+   *
+   * @param deleteRequests list of DeleteRequest to be performed in bulk.
+   *
+   * @return A Mono representing success of failure of the collective operation.
+   */
+  default Mono<Void> bulkDelete(List<DeleteRequest> deleteRequests) {
+    return Flux.fromIterable(deleteRequests)
+        .flatMap(this::delete)
+        .then(); // convert this Flux to a Mono
+  }
+
+  /**
+   * Set the value of many keys at once.
+   *
+   * @param setRequests list of SetRequests to be performed in bulk.
+   *
+   * @return A Mono representing success of failure of the collective operation.
+   */
+  default Mono<Void> bulkSet(List<SetRequest> setRequests) {
+    return Flux.fromIterable(setRequests)
+        .flatMap(this::set)
+        .then(); // convert this Flux to a Mono
+  }
 }
